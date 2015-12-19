51fc69fc2e3c849979a0bc15d4089b1ba02a20f3
Marc Chantreux <khatar@phear.org>
2015-12-19 02:28:42 +0100

    document osiris.zsh

--- lib/osiris.zsh
+++ lib/osiris.zsh
@@ -1,9 +1,16 @@
+: <<'=cut'
 
-# keep captive portal aware of your presence
-# so you can close your browser without loosing connection
+=head1 osiris/wifi
+
+keep captive portal aware of your presence by fetching the
+wifi page every 1 seconds so you can close your browser
+without loosing connection. 
+
+=cut
+
 osiris/wifi () {
     local login passwd 
     < ~/.osiris_credentials | fill login passwd
     local url=https://wifi.pcap.u-strasbg.fr/bin/connexion 
-    while {true} {shush wget --http-user=$login --http-password=$passwd $url}
+    while {sleep 1} {shush wget --http-user=$login --http-password=$passwd $url}
 }
