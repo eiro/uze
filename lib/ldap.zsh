7f8210f90d94ef7304b6e3aadc2af69e1df84698
Marc Chantreux <khatar@phear.org>
2016-01-06 11:04:04 +0100

    add ldap filters helpers

--- lib/ldap.zsh
+++ lib/ldap.zsh
@@ -23,16 +23,31 @@ prepare
 
 ldap/filter/minify () {
     perl -MNet::LDAP::Filter -0 -nE '
-    
         s/  (?(DEFINE)
             (?<ws_or_eol> [\v\h]+))
 
             ^ (?&ws_or_eol)
             | (?&ws_or_eol) $
         //xmsg;
-
         say Net::LDAP::Filter 
         -> new( $_ )
         -> as_string
     ' "$@"
-} 
+}
+
+() {
+    local fn sym
+    for fn sym ( or \| and \& ) eval '
+    ldap/filter/'$fn'- () {
+        sed ''
+        s/.*/(''${1-}''&)/
+        1s/^/('$sym'/
+        $s/$/)/'' }
+
+    ldap/filter/'$fn'  () {
+        argv=( "('$sym'" "($1${^@[2,-1]})" ")" )
+        print "${(j::)argv}"
+    }
+
+    '
+}
