0d0a021321c6bf31a7f7d0faf75271b9d2d700b8
Marc Chantreux <khatar@phear.org>
2016-03-16 16:40:23 +0100

    unistra/annuaire is now a uze module

--- lib/unistra/annuaire.zsh
+++ lib/unistra/annuaire.zsh
@@ -3,18 +3,18 @@
 =head1 query http://annaire.unistra.fr/chercher
 
 is a form to query the University of Strasbourg Yellow Page
-this uze provide wrappers and filters on it 
+this uze provide wrappers and filters on it
 
 unistra/annuaire/search lynx dumps a result of a search
 
-    parameters are 
+    parameters are
     p: firstname (prenom)
     n: lastname  (name)
-    s: structure name 
+    s: structure name
 
 in most cases, you don't want the whole page, so we have helpers and filters:
 unistra/annuaire/cards for complete informations about found people
-unistra/annuaire/emails for just emails 
+unistra/annuaire/emails for just emails
 
 as example, you can find me with
 unistra/annuaire/cards p marc m chant
@@ -23,24 +23,18 @@ unistra/annuaire/cards p marc m chant
 
 uze lynx
 
-function () {
-    local m=$1
-    shift
-
-    $m/search () { lynx/dump "http://annuaire.unistra.fr/chercher" "$@" }
-
-    $m/filter/cards () {
-        perl -lnE 'print if m{ ^\[\d\][^&]+ $ }x ...  m{ ^\s*$ }x'
-    }
-
-    $m/filter/emails () { 
+() {
+    $1/search () { lynx/dump "http://annuaire.unistra.fr/chercher" "$@" }
+    $1/filter/cards () {
+        perl -lnE 'print if m{ ^\[\d\][^&]+ $ }x ...  m{ ^\s*$ }x' }
+    $1/filter/emails () {
         sed -nr '
             /Informations complémentaires/ {q}
             /@/ { s/.*]//p }
-        ' 
+        '
     }
 
     local f
     for f (cards emails)
-        eval "$m/$f () { $m/search "'"$@"'" | $m/filter/$f }"
-} ${0#uze/} "$@"
+        eval "$1/$f () { $1/search "'"$@"'" | $1/filter/$f }"
+} $__PACKAGE__
