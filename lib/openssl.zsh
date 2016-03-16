63326d1c1f4d8ad50fe06b1ddfaa398f8acbf2a9
Marc Chantreux <khatar@phear.org>
2016-03-16 16:37:00 +0100

    add usage message to getcert

--- lib/openssl.zsh
+++ lib/openssl.zsh
@@ -1,5 +1,6 @@
 openssl/getcert () {
-    :| openssl s_client -connect $1:${2:-993} 2>/dev/null |
+    :| openssl s_client -connect \
+        ${1?usage: $0 hostname [port=993]}:${2:-993} 2>/dev/null |
 	sed -n '/BEGIN/,/END/p'
 }
 
