e08a452a540e96ae9cd617e283e502bc94116ce1
Marc Chantreux <khatar@phear.org>
2016-03-16 16:29:07 +0100

    add tmux module

--- lib/tmux.zsh
+++ lib/tmux.zsh
@@ -1,8 +1,27 @@
-tmux/smart () {
+: <<'=cut'
+
+=head1 tmux helpers
+
+=head2 tmux/simple
+
+ease the use of tmux simplest (and most common) usecases.
+recommended alias: C<t>.
+
+to list the names of existing sessions:
+
+    t
+
+to attach an existing session with a name starting with 'foo':
+
+    t foo
+
+to create a new session 'foo':
+
+=cut
+
+tmux/simple () {
     (( $+1 )) ||
         {tmux ls -F '#S'; return} 
     shush tmux att -t $1 ||
         tmux new -s $1
 }
-
-
