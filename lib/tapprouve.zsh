58c314644c4379ec8925fcaa3cad374058c706b3
Marc Chantreux <khatar@phear.org>
2018-04-30 12:09:36 +0200

    add tapprouve -h

--- lib/tapprouve.zsh
+++ lib/tapprouve.zsh
@@ -1,5 +1,10 @@
 tapprouve () {
-    # tapprouve t/*.tap > t.html
+
+    [[ ${1:-} == -h ]] && {
+        l 'tapprouve t/*.tap > t.html'
+        return
+    }
+
     prove -m -Q \
     --formatter=TAP::Formatter::HTML \
     --source File "$@"
